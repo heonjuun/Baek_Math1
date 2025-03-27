4bd864aac869ef5215bb25d4aef47c5c17ad73c8
heonjunkim <s01077314486@gmail.com>
2025-03-27 14:24:26 +0900

    formation1

--- 2745.c
+++ 2745.c
@@ -2,6 +2,17 @@
 //#include<stdlib.h>
 //
 //int main() {
-//
+//	char input[100000];
+//	int n;
+//	int sum = 0;
+//	scanf("%s %d", input, &n);
+//	for (int i = 0; input[i] != '\0'; i++) //문자열의 끝 (\0)이 나올 때까지 반복
+//	{
+//		if (input[i] >= 'A' && 'Z' >= input[i])
+//			sum = sum * n + (input[i] - 'A' + 10);
+//		else
+//			sum = sum * n + (input[i] - '0');		
+//	}
+//	printf("%d", sum);
 //	return 0;
 //}
--- honer'srule.c
+++ honer'srule.c
@@ -1,28 +1,28 @@
-#include<stdio.h>
-char input[100]; //변환할 숫자를 문자열 형태로 저장하는 배열 (최대 99자리, 마지막은 '\0')
-int x;
-long long honer(int x)
-{
-	long long sum = 0;
-
-	for (int i = 0; input[i] != '\0'; i++) //// 문자열을 한 글자씩 검사
-	{
-		if (input[i] >= 'A' && input[i] <= 'Z') //알파벳일 경우
-			sum = sum * x + (input[i] - 'A' + 10); //// 'A'부터 10, 'B'는 11, ..., 'Z'는 35
-		else //// 숫자(0~9)일 경우
-			sum = sum * x + (input[i] - '0'); //// '0'~'9'는 그대로 숫자로 변환
-	}
-	return sum;
-	
-	//else if (input[i] >= 'a' && input[i] <= 'z')
-	//	sum = sum * x + (input[i] - 'a' + 10); // 소문자도 처리
-
-}
-
-int main()
-{
-	scanf("%s", input);//변환할 숫자
-	scanf("%d", &x);//몇진법수
-	printf("result : %lld \n", honer(x));
-	return 0;
-}
+//#include<stdio.h>
+//char input[100]; //변환할 숫자를 문자열 형태로 저장하는 배열 (최대 99자리, 마지막은 '\0')
+//int x;
+//long long honer(int x)
+//{
+//	long long sum = 0;
+//
+//	for (int i = 0; input[i] != '\0'; i++) //// 문자열을 한 글자씩 검사
+//	{
+//		if (input[i] >= 'A' && input[i] <= 'Z') //알파벳일 경우
+//			sum = sum * x + (input[i] - 'A' + 10); //// 'A'부터 10, 'B'는 11, ..., 'Z'는 35
+//		else //// 숫자(0~9)일 경우
+//			sum = sum * x + (input[i] - '0'); //// '0'~'9'는 그대로 숫자로 변환
+//	}
+//	return sum;
+//	
+//	//else if (input[i] >= 'a' && input[i] <= 'z')
+//	//	sum = sum * x + (input[i] - 'a' + 10); // 소문자도 처리
+//
+//}
+//
+//int main()
+//{
+//	scanf("%s", input);//변환할 숫자
+//	scanf("%d", &x);//몇진법수
+//	printf("result : %lld \n", honer(x));
+//	return 0;
+//}
